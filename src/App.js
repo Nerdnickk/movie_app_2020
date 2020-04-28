29dd11c805a89cf4e3cc93fd5dc0f98032f290b9
Nerdnickk <56470910+Nerdnickk@users.noreply.github.com>
2020-04-29 01:51:16 +0900

    react third

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-const musicLike =[
-  {
-    id: 1,
-    name: "hiphop",
-    image: "https://image.shutterstock.com/image-vector/vector-hiphop-graffiti-doodle-art-600w-1416068528.jpg",
-    rating: 4.5
-  },
-  {
-    id: 2,
-    name: "jazz",
-    image: "http://www.janovecjazzband.cz/images/3-foto.png",
-    rating: 3
-  },
-  {
-    id: 3,
-    name: "oldschool",
-    image: "https://previews.123rf.com/images/rawpixel/rawpixel1706/rawpixel170660029/89205497-group-of-dj-mixing-the-classic-oldschool-music-vinyl-record.jpg",
-    rating: 2
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const {
+      data:{
+        data:{movies}
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({movies, isLoading: false});
   }
-]
 
-function Humble({name, picture, rating}){
-  return( 
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  )
-}
-
-Humble.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-function App() {
+  componentDidMount(){
+    this.getMovies();
+  }
+  
+  render(){
+    const {isLoading, movies} = this.state;
   return (
-    <div>
-      <h1>Hello</h1>
-      {musicLike.map(dish => (
-        <Humble key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+  <section className="container">
+    {isLoading ? (
+      <div className="loader">
+        <span className="loader__text">Loading....</span>
+      </div>
+    ) : (
+      <div className="movies">
+      {movies.map(movie => (
+        <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>
       ))}
-    </div>
-  );
+      </div>
+      )}
+  </section>
+    )    
+  }
 }
 
 export default App;
